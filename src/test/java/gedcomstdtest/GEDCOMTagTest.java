011f28f7ba9a2d4c1d9ced2550e56607bfa8d58e
UndeadScythes <daveyognaut2@gmail.com>
2013-09-20 10:27:15 +0100

    Add JUnit test

---
+++ src/test/java/gedcomstdtest/GEDCOMTagTest.java
@@ -0,0 +1,16 @@
+package gedcomstdtest;
+
+import com.undeadscythes.gedcomstd.*;
+import org.junit.*;
+
+/**
+ * @author UndeadScythes
+ */
+public class GEDCOMTagTest extends Assert {
+    @Test
+    public void testGetByTag() {
+        for (GEDTag tag : GEDTag.values()) {
+            assertEquals("getByName", tag, GEDTag.getByName(tag.getTag()));
+        }
+    }
+}
