c7e22c1848544af08004d5af4e070d5f76febdad
UndeadScythes <daveyognaut2@gmail.com>
2013-09-22 03:15:16 +0100

    Implement SortByDate comparator

--- src/main/java/com/undeadscythes/genebase/comparator/SortByDate.java
+++ src/main/java/com/undeadscythes/genebase/comparator/SortByDate.java
@@ -1,7 +1,7 @@
 package com.undeadscythes.genebase.comparator;
 
+import com.undeadscythes.genebase.structure.Date;
 import com.undeadscythes.genebase.structure.*;
-import com.undeadscythes.metaturtle.*;
 import java.util.*;
 
 /**
@@ -9,7 +9,7 @@ import java.util.*;
  *
  * @author UndeadScythes
  */
-public final class SortByDate implements Comparator<Metadata> {
+public final class SortByDate implements Comparator<Event> {
     /**
      * Earliest {@link Event}s come first.
      */
@@ -24,14 +24,26 @@ public final class SortByDate implements Comparator<Metadata> {
         DECREASING = new SortByDate(false);
     }
 
+    /**
+     * @see Comparable#compareTo(Object) Comparable.compareTo(Date)
+     */
+    public static int compare(final Date date1, final Date date2) {
+        if (date1.year != date2.year) return date1.year - date2.year;
+        if (date1.month != date2.month) return date1.month - date2.month;
+        if (date1.quarter != date2.quarter) return date1.quarter - date2.quarter;
+        if (date1.day != date2.day) return date1.day - date2.day;
+        return 0;
+    }
+
     private final boolean increasing;
 
     private SortByDate(final boolean increasing) {
         this.increasing = increasing;
-        //TODO: Implement me
     }
 
-    public int compare(final Metadata data1, final Metadata data2) {
-        return 0; //TODO: Implement me
+    public int compare(final Event event1, final Event event2) {
+        final Date date1 = event1.getDate();
+        final Date date2 = event2.getDate();
+        return increasing ? compare(date1, date2) : compare(date2, date1);
     }
 }
