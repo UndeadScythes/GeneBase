1ae5c606433b08ba0ef82ec4be21b391870bbc54
UndeadScythes <daveyognaut2@gmail.com>
2013-09-22 03:13:37 +0100

    Add GEDCOM tag formal names to GEDTag

--- src/main/java/com/undeadscythes/genebase/gedcom/GEDTag.java
+++ src/main/java/com/undeadscythes/genebase/gedcom/GEDTag.java
@@ -1,8 +1,7 @@
 package com.undeadscythes.genebase.gedcom;
 
 import com.undeadscythes.genebase.exception.*;
-import com.undeadscythes.genebase.specific.*;
-import com.undeadscythes.metaturtle.*;
+import com.undeadscythes.metaturtle.metadata.*;
 import java.util.*;
 
 /**
@@ -10,40 +9,205 @@ import java.util.*;
  *
  * @author UndeadScythes
  */
-public enum GEDTag implements MetaType {
-    ABBR, ADDR, ADR1, ADR2, ADOP, AFN, AGE, AGNC, ALIA, ANCE, ANCI, ANUL, ASSO, AUTH,
-    BAPL, BAPM, BARM, BASM, BIRT, BLES, BLOB, BURI,
-    CALN, CAST, CAUS, CENS, CHAN, CHAR, CHIL, CHR, CHRA, CITY, CONC, CONF, CONL, CONT, COPR, CORP, CREM, CTRY,
-    DATA, DATE, DEAT, DESC, DESI, DEST, DIV, DIVF, DSCR,
-    EDUC, EMIG, ENDL, ENGA, EVEN,
-    FAM, FAMC, FAMF, FAMS, FCOM, FILE, FORM,
-    GEDC, GIVN, GRAD,
-    HEAD, HUSB,
-    IDNO, IMMI, INDI,
-    LANG, LEGA,
-    MARB, MARC, MARL, MARR, MARS, MEDI,
-    NAME, NATI, NATU, NCHI, NICK, NMR, NOTE, NPFX, NSFX,
-    OBJE, OCCU, ORDI, ORDN,
-    PAGE, PEDI, PHON, PLAC, POST, PROB, PROP, PUBL,
-    QUAY,
-    REFN, RELA, RELI, REPO, RESI, RESN, RETI, RFN, RIN, ROLE,
-    SEX, SLGC, SLGS, SOUR, SPFX, SSN, STAE, STAT, SUBM, SUBN, SURN,
-    TEMP, TEXT, TIME, TITL, TRLR, TYPE,
-    VERS,
-    WIFE,
-    WILL,
+public enum GEDTag implements Property {
+    ABBR("Abbreviation"),
+    ADDR("Address"),
+    ADR1("Address 1"),
+    ADR2("Address 2"),
+    ADOP("Adoption"),
+    AFN("Ancestral file number"),
+    AGE("Age"),
+    AGNC("Agency"),
+    ALIA("Alias"),
+    ANCE("Ancestors"),
+    ANCI("Ancestor interest"),
+    ANUL("Annulment"),
+    ASSO("Associates"),
+    AUTH("Author"),
+    BAPL("LDS baptism"),
+    BAPM("Baptism"),
+    BARM("Bar mitzvah"),
+    BASM("Bat mitzvah"),
+    BIRT("Birth"),
+    BLES("Blessing"),
+    /**
+     * Removed in 5.5.1.
+     *
+     * @see #OBJE OBJE
+     */
+    @Deprecated
+    BLOB("Binary object"),
+    BURI("Burial"),
+    CALN("Call number"),
+    CAST("Caste"),
+    CAUS("Cause"),
+    CENS("Census"),
+    CHAN("Change"),
+    CHAR("Character"),
+    CHIL("Child"),
+    CHR("Christening"),
+    CHRA("Adult Christening"),
+    CITY("City"),
+    /**
+     * Added in 5.6.
+     */
+    CLNDR("Calendar type"),
+    CONF("Confirmation"),
+    CONL("LDS confirmation"),
+    COPR("Copyright"),
+    CORP("Corporate"),
+    CREM("Cremation"),
+    CTRY("Country"),
+    DATA("Data"),
+    DATE("Date"),
+    DEAT("Death"),
+    DESC("Descendants"),
+    DESI("Descendant interest"),
+    DEST("Destination"),
+    DIV("Divorce"),
+    DIVF("Divorce filed"),
+    DSCR("Physical description"),
+    EDUC("Education"),
+    /**
+     * Added in 5.5.1.
+     */
+    EMAIL("Email"),
+    EMIG("Emigration"),
+    ENDL("LDS endowment"),
+    ENGA("Engagement"),
+    EVEN("Event"),
+    /**
+     * Added in 5.5.1.
+     */
+    FACT("Fact"),
+    FAMC("Family child"),
+    FAMF("Family file"),
+    FAMS("Family spouse"),
+    /**
+     * Added in 5.5.1.
+     */
+    FAX("Facimilie"),
+    FCOM("First communion"),
+    FILE("File"),
+    /**
+     * Added in 5.5.1.
+     */
+    FONE("Phonetic"),
+    FORM("Format"),
+    GEDC("GEDCOM"),
+    GIVN("Given name"),
+    GRAD("Graduation"),
+    HUSB("Husband"),
+    IDNO("ID number"),
+    IMMI("Immigration"),
+    LANG("Language"),
+    /**
+     * Added in 5.5.1.
+     */
+    LATI("latitude"),
+    /**
+     * Removed in 5.6.
+     */
+    @Deprecated
+    LEGA("Legatee"),
+    /**
+     * Added in 5.5.1.
+     */
+    LONG("Longitude"),
+    /**
+     * Added in 5.5.1.
+     */
+    MAP("Map"),
+    MARB("Marriage bann"),
+    MARC("Marriage contract"),
+    MARL("Marriage license"),
+    MARR("Marriage"),
+    MARS("Marriage settlement"),
+    MEDI("media"),
+    NAME("Name"),
+    NATI("Nationality"),
+    NATU("naturalization"),
+    NCHI("Number of children"),
+    NICK("Nickname"),
+    NMR("Number of marriages"),
+    NOTE("Note"),
+    NPFX("Name prefix"),
+    NSFX("Name suffix"),
+    OBJE("Object"),
+    OCCU("Occupation"),
+    ORDI("Ordinance"),
+    ORDN("Ordination"),
+    PAGE("Page"),
+    PEDI("Pedigree"),
+    PHON("Phone"),
+    PLAC("Place"),
+    POST("Postal code"),
+    PROB("Probate"),
+    PROP("Property"),
+    PUBL("Publication"),
+    QUAY("Quality of data"),
+    REFN("Reference"),
+    RELA("Relationship"),
+    RELI("Religion"),
+    RESI("Residence"),
+    RESN("Restriction"),
+    RETI("Retirement"),
+    RFN("Record file number"),
+    RIN("Record ID number"),
+    ROLE("Role"),
+    /**
+     * Added in 5.5.1.
+     */
+    ROMN("Romanized"),
+    SEX("Gender"),
+    SLGC("LDS sealing child"),
+    SLGS("LDS sealing spouse"),
+    SOUR("Source"),
+    SPFX("Family name prefix"),
+    SSN("Social security number"),
+    STAE("State"),
+    STAT("Status"),
+    SUBM("Submitter"),
+    SUBN("Submission"),
+    SURN("Family name"),
+    TEMP("Temple"),
+    TEXT("Text"),
+    TIME("Time"),
+    TITL("Title"),
+    TYPE("Type"),
+    /**
+     * Added in 5.6
+     */
+    URL("URL"),
+    VERS("Version"),
+    /**
+     * Added in 5.6.
+     */
+    WAC("LDS washing and clothing"),
+    WIFE("Wife"),
+    WILL("Will"),
+    /**
+     * Added in 5.5.1, removed in 5.6.
+     *
+     * @see #URL URL
+     */
+    @Deprecated
+    WWW("Website");
 
-    MAP, // 5.5.1
-    LATI, // 5.5.1
-    LONG, // 5.5.1
-    FONE, // 5.5.1
-    ROMN, // 5.5.1
-    FACT, // 5.5.1
-    FAX, // 5.5.1
-    WWW, // 5.5.1
-    EMAIL; // 5.5.1
+    /**
+     * The formal name of this {@link Property}.
+     */
+    public final String formal;
 
-    private final static List<CustomTag> CUSTOM_TAGS = new ArrayList<CustomTag>(0);
+    private GEDTag(final String formal) {
+        this.formal = formal;
+    }
+
+    private final static List<Property> CUSTOM_TAGS = new ArrayList<Property>(0);
+
+    static {
+        CUSTOM_TAGS.addAll(Arrays.asList(values()));
+    }
 
     /**
      * Add a {@link CustomTag}.
@@ -53,19 +217,28 @@ public enum GEDTag implements MetaType {
     }
 
     /**
-     * Get the printable name of this {@link GEDTag}.
+     * Get an immutable list of all {@link GEDTag}s and {@link CustomTag}s.
      */
-    public String getTag() {
-        return name();
+    public static List<Property> getValues() {
+        return Collections.unmodifiableList(CUSTOM_TAGS);
     }
 
     /**
-     * Get a {@link GEDTag} matching a given name.
+     * Get a {@link Property} matching a given name.
      */
-    public static GEDTag getByName(final String name) {
-        for (GEDTag tag : values()) {
-            if (tag.getTag().equals(name)) return tag;
+    public static Property getByName(final String name) {
+        for (Property tag : getValues()) {
+            if (tag.getString().equals(name)) return tag;
         }
         throw new NoValidTagException(name);
+    }
+
+    public boolean equals(final String string) {
+        return getString().equals(string);
+    }
+
+
+    public String getString() {
+        return name();
     }
 }
