4dc3147069721b92c056792092e429de2a320c45
UndeadScythes <daveyognaut2@gmail.com>
2013-09-22 03:15:26 +0100

    Implement SortByName comparator

--- src/main/java/com/undeadscythes/genebase/comparator/SortByName.java
+++ src/main/java/com/undeadscythes/genebase/comparator/SortByName.java
@@ -8,7 +8,7 @@ import java.util.*;
  *
  * @author UndeadScythes
  */
-public class SortByName implements Comparator<Individual> {
+public final class SortByName implements Comparator<Individual> {
     /**
      * Alphabetical.
      */
@@ -30,6 +30,11 @@ public class SortByName implements Comparator<Individual> {
     }
 
     public int compare(final Individual indi1, final Individual indi2) {
-        return 0; //TODO: Implement me
+        final String family1 = indi1.getFamilyName();
+        final String family2 = indi2.getFamilyName();
+        if (!family1.equals(family2)) return ascending ? family1.compareTo(family2) : family2.compareTo(family1);
+        final String given1 = indi1.getGivenName();
+        final String given2 = indi2.getGivenName();
+        return ascending ? given1.compareTo(given2) : given2.compareTo(given1);
     }
 }
