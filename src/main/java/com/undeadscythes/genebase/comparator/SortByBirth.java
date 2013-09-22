3a8fdf439b876e58e241c49227c7f29b5808c308
UndeadScythes <daveyognaut2@gmail.com>
2013-09-22 03:15:03 +0100

    Implement SortByBirth comparator

--- src/main/java/com/undeadscythes/genebase/comparator/SortByBirth.java
+++ src/main/java/com/undeadscythes/genebase/comparator/SortByBirth.java
@@ -1,6 +1,9 @@
 package com.undeadscythes.genebase.comparator;
 
+import com.undeadscythes.genebase.gedcom.*;
 import com.undeadscythes.genebase.record.*;
+import com.undeadscythes.genebase.structure.*;
+import com.undeadscythes.metaturtle.exception.*;
 import java.util.*;
 
 /**
@@ -23,13 +26,25 @@ public final class SortByBirth implements Comparator<Individual> {
         DESCENDING = new SortByBirth(false);
     }
 
-    private boolean ascending;
+    private final boolean ascending;
 
     private SortByBirth(final boolean ascending) {
         this.ascending = ascending;
     }
 
     public int compare(final Individual indi1, final Individual indi2) {
-        return 0; //TODO: Implement me
+        final Event birth1;
+        final Event birth2;
+        try {
+            birth1 = (Event)indi1.getFirst(GEDTag.BIRT);
+        } catch (NoMetadataSetException ex) {
+            return ascending ? -1 : 1;
+        }
+        try {
+            birth2 = (Event)indi1.getFirst(GEDTag.BIRT);
+        } catch (NoMetadataSetException ex) {
+            return ascending ? 1 : -1;
+        }
+        return ascending ? SortByDate.INCREASING.compare(birth1, birth2) : SortByDate.DECREASING.compare(birth1, birth2);
     }
 }
