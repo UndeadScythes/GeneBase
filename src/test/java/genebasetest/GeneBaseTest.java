b30e824f357e298fcaf4291b130fb3a1794e75ce
UndeadScythes <daveyognaut2@gmail.com>
2013-09-22 05:35:07 +0100

    Remove redundant GEDCOM torture tests

--- src/test/java/genebasetest/GeneBaseTest.java
+++ src/test/java/genebasetest/GeneBaseTest.java
@@ -13,30 +13,12 @@ import org.junit.*;
  */
 public class GeneBaseTest {
     @Test
-    public void testTGC551() throws ParsingException {
-        final GeneBase geneBase = new GeneBase(new GEDCOM("src/test/resources/TGC551.ged"));
+    public void testTGC55C() throws ParsingException {
+        final GeneBase geneBase = new GeneBase(new GEDCOM("src/test/resources/TGC55C.ged"));
         assertFalse("isEmpty()", geneBase.isEmpty());
         final TipScript tipScript = new TipScript(Logger.getLogger(getClass().getName()), "Test: ");
         tipScript.openFile("test.ged");
         geneBase.dump(tipScript);
         tipScript.closeFile();
-    }
-
-    @Test
-    public void testTGC551LF() throws ParsingException {
-        final GeneBase geneBase = new GeneBase(new GEDCOM("src/test/resources/TGC551LF.ged"));
-        assertFalse("isEmpty()", geneBase.isEmpty());
-    }
-
-    @Test
-    public void testTGC55C() throws ParsingException {
-        final GeneBase geneBase = new GeneBase(new GEDCOM("src/test/resources/TGC55C.ged"));
-        assertFalse("isEmpty()", geneBase.isEmpty());
-    }
-
-    @Test
-    public void testTGC55CLF() throws ParsingException {
-        final GeneBase geneBase = new GeneBase(new GEDCOM("src/test/resources/TGC55CLF.ged"));
-        assertFalse("isEmpty()", geneBase.isEmpty());
     }
 }
