0ab0df4d0fb90130dd695488f43040b67b2f9871
UndeadScythes <daveyognaut2@gmail.com>
2013-09-22 03:10:15 +0100

    Rename GEDCOMTagTest to GEDTagTest

---
+++ src/test/java/genebasetest/GEDTagTest.java
@@ -0,0 +1,17 @@
+package genebasetest;
+
+import com.undeadscythes.genebase.gedcom.*;
+import com.undeadscythes.metaturtle.metadata.*;
+import org.junit.*;
+
+/**
+ * @author UndeadScythes
+ */
+public class GEDTagTest extends Assert {
+    @Test
+    public void testGetByTag() {
+        for (Property tag : GEDTag.getValues()) {
+            assertEquals("getByName", tag, GEDTag.getByName(tag.getString()));
+        }
+    }
+}
