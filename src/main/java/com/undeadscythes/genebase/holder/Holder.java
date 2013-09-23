2dfd8f06aefba700d17cafd6217affee2f8079db
UndeadScythes <daveyognaut2@gmail.com>
2013-09-23 11:59:04 +0100

    Add additional Holder constructors

--- src/main/java/com/undeadscythes/genebase/holder/Holder.java
+++ src/main/java/com/undeadscythes/genebase/holder/Holder.java
@@ -1,6 +1,10 @@
 package com.undeadscythes.genebase.holder;
 
 import com.undeadscythes.gedform.*;
+import com.undeadscythes.genebase.exception.*;
+import com.undeadscythes.genebase.gedcom.*;
+import com.undeadscythes.genebase.structure.*;
+import com.undeadscythes.metaturtle.*;
 import com.undeadscythes.metaturtle.metadata.*;
 import com.undeadscythes.tipscript.*;
 
@@ -13,13 +17,38 @@ public class Holder extends Metadata {
     private static final long serialVersionUID = 1L;
 
     /**
+     * Load a generic {@link Metadatable} with the values of the given
+     * {@link Cluster}.
+     */
+    public static void load(final Metadatable meta, final Cluster cluster) {
+        while (cluster.hasNext()) {
+            final Cluster next = cluster.pullCluster();
+            Property property;
+            try {
+                property = GEDTag.getByName(next.getTag());
+            } catch (NoValidTagException ex) {
+                final CustomTag custom = new CustomTag(next.getTag());
+                GEDTag.addTag(custom);
+                property = custom;
+            }
+            if (GEDTag.DATE.equals(next.getTag())) {
+                meta.add(Date.parseDate(next));
+            } else if (Event.EVENT_TAGS.contains(property)) {
+                meta.add(new Event(next));
+            } else if (GEDTag.PLAC.equals(next.getTag())) {
+                meta.add(new Place(next));
+            } else {
+                meta.add(new Holder(next));
+            }
+        }
+    }
+
+    /**
      * Load this {@link Holder} with the data in the given {@link Cluster}.
      */
     public Holder(final Cluster cluster) {
         this(cluster.pullHead().tag, cluster.pullHead().value);
-        while (cluster.hasNext()) {
-            add(new Holder(cluster.pullCluster()));
-        }
+        load(this, cluster);
     }
 
     /**
@@ -30,11 +59,32 @@ public class Holder extends Metadata {
     }
 
     /**
+     * @see Metadata#Metadata(String, String)
+     */
+    public Holder(final Property property, final String value) {
+        super(property.getString(), value);
+    }
+
+    /**
+     * Specific constructor for when a sub class overrides the
+     * {@link #getValue() getValue()} method.
+     *
+     * @see Metadata#Metadata(String, String)
+     */
+    public Holder(final Property property) {
+        super(property.getString(), "OVERRIDDEN");
+    }
+
+    /**
      * Output the contents of this {@link Holder} to the given
      * {@link TipScript} file.
      */
     public void dump(final TipScript out, final String prefix) {
-        out.printf(prefix + getProperty() + ": " + getValue());
+        final StringBuilder newLine = new StringBuilder("\n  ");
+        for (int i = 0; i < prefix.length() + getProperty().length(); i++) {
+            newLine.append(" ");
+        }
+        out.printf(prefix + getProperty() + ": " + getValue().replace("\n", newLine.toString()));
         for (Metadata data : this) {
             ((Holder)data).dump(out, "  " + prefix);
         }
