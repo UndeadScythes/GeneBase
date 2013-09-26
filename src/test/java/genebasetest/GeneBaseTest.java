daf994850e3cea99acb40ba270a01962a1e9a506
UndeadScythes <daveyognaut2@gmail.com>
2013-09-26 11:57:34 +0100

    Add get by path test

--- src/test/java/genebasetest/GeneBaseTest.java
+++ src/test/java/genebasetest/GeneBaseTest.java
@@ -1,24 +1,31 @@
 package genebasetest;
 
-import com.undeadscythes.gedform.exception.*;
-import com.undeadscythes.genebase.*;
-import com.undeadscythes.genebase.gedcom.*;
-import com.undeadscythes.tipscript.*;
-import java.util.logging.*;
-import static org.junit.Assert.*;
-import org.junit.*;
+import com.undeadscythes.gedform.exception.ParsingException;
+import com.undeadscythes.genebase.GeneBase;
+import com.undeadscythes.genebase.gedcom.GEDCOM;
+import com.undeadscythes.genebase.gedcom.GEDTag;
+import com.undeadscythes.genebase.gedcom.RecordType;
+import com.undeadscythes.metaturtle.exception.NoMetadataSetException;
+import com.undeadscythes.metaturtle.exception.NoUniqueMetaException;
+import com.undeadscythes.metaturtle.unique.UID;
+import com.undeadscythes.tipscript.TipScript;
+import java.util.logging.Logger;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
 
 /**
  * @author UndeadScythes
  */
 public class GeneBaseTest {
     @Test
-    public void testTGC55C() throws ParsingException {
+    public void testTGC55C() throws ParsingException, NoUniqueMetaException, NoMetadataSetException {
         final GeneBase geneBase = new GeneBase(new GEDCOM("src/test/resources/TGC55C.ged"));
         assertFalse(geneBase.isEmpty());
         final TipScript tipScript = new TipScript(Logger.getLogger(getClass().getName()), "Test: ");
         tipScript.openFile("test.ged");
         geneBase.dump(tipScript);
         tipScript.closeFile();
+        assertEquals("Charlie Accented /ANSEL/", geneBase.getUniqueMeta(RecordType.INDI, new UID("I14")).getFirst(GEDTag.NAME).getValue());
     }
 }
