2894026404dcaf7447b5b09999a94a1288a07ee3
UndeadScythes <daveyognaut2@gmail.com>
2013-09-26 11:58:10 +0100

    Implement Event constructor

--- src/main/java/com/undeadscythes/genebase/structure/Event.java
+++ src/main/java/com/undeadscythes/genebase/structure/Event.java
@@ -1,12 +1,15 @@
 package com.undeadscythes.genebase.structure;
 
-import com.undeadscythes.gedform.*;
-import com.undeadscythes.genebase.comparator.*;
-import com.undeadscythes.genebase.gedcom.*;
-import com.undeadscythes.genebase.holder.*;
-import com.undeadscythes.genebase.structure.Date;
-import com.undeadscythes.metaturtle.metadata.*;
-import java.util.*;
+import com.undeadscythes.gedform.Cluster;
+import com.undeadscythes.genebase.comparator.SortByDate;
+import com.undeadscythes.genebase.gedcom.GEDTag;
+import com.undeadscythes.genebase.gedcom.NamedTag;
+import com.undeadscythes.genebase.holder.Holder;
+import com.undeadscythes.metaturtle.exception.NoMetadataSetException;
+import com.undeadscythes.metaturtle.metadata.Property;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * An {@link Event} is a property that has both a time and place.
@@ -16,17 +19,28 @@ import java.util.*;
 public class Event extends Holder {
     private static final long serialVersionUID = 1L;
 
-    public static final List<Property> EVENT_TAGS = new ArrayList<Property>(Arrays.asList(GEDTag.BIRT, GEDTag.EVEN));
+    public static final List<Property> EVENT_TAGS = new ArrayList<Property>(Arrays.asList(
+            GEDTag.BIRT,
+            GEDTag.DEAT,
+            GEDTag.EVEN,
+            GEDTag.RESI));
 
     private Date date;
     private Place place;
-    private GEDTag type;
+    private NamedTag type;
 
-    /**
-     * Create an {@link Event} with a given {@link Cluster}.
-     */
     public Event(final Cluster cluster) {
-        super(cluster); //TODO: Implement me
+        super(cluster);
+        this.type = GEDTag.getByName(cluster.getTag());
+        cluster.reset();
+        while (cluster.hasNext()) {
+            final Cluster next = cluster.pullCluster();
+            if (GEDTag.DATE.equals(next.getTag())) {
+                date = Date.parseDate(next);
+            } else if (GEDTag.PLAC.equals(next.getTag())) {
+                place = new Place(next);
+            }
+        }
     }
 
     /**
@@ -46,7 +60,8 @@ public class Event extends Holder {
     /**
      * Get the {@link Date} this event occurred.
      */
-    public Date getDate() {
+    public Date getDate() throws NoMetadataSetException {
+        if (date == null) throw new NoMetadataSetException("DATE");
         return date;
     }
 
