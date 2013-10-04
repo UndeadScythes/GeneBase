0bfc779d2440972f8c652cfc21480df801e0a733
UndeadScythes <daveyognaut2@gmail.com>
2013-10-04 13:36:48 +0100

    Add method to get Tag type

--- src/main/java/com/undeadscythes/genebase/gedcom/NamedTag.java
+++ src/main/java/com/undeadscythes/genebase/gedcom/NamedTag.java
@@ -1,8 +1,11 @@
 package com.undeadscythes.genebase.gedcom;
 
-import com.undeadscythes.metaturtle.metadata.*;
+import com.undeadscythes.genebase.gedcom.GEDTag.TagType;
+import com.undeadscythes.metaturtle.metadata.Property;
 
 /**
+ * A tag with a specific name.
+ *
  * @author UndeadScythes
  */
 public interface NamedTag extends Property {
@@ -10,4 +13,9 @@ public interface NamedTag extends Property {
      * Get the formal name of this {@link NamedTag}.
      */
     String getFormal();
+
+    /**
+     * Get the type of this NamedTag.
+     */
+    TagType getType();
 }
